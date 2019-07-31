722074a625dfea23e78245f91db58be44402981b
hanbo <hanbov5@gmail.com>
2019-07-31 20:49:47 +0800

    no message

--- app/uicase/ios_engine.py
+++ app/uicase/ios_engine.py
@@ -1,29 +1,25 @@
-
 import unittest
 import os
 from appium import webdriver
-from time  import sleep
+from time import sleep
 
 
-class  appiumSimpleTezt (unittest.TestCase):
+class appiumSimpleTezt(unittest.TestCase):
 
-    def  setUp(self):
-
+    def setUp(self):
         self.driver = webdriver.Remote(
-            command_executor = 'http://127.0.0.1:4723/wd/hub',
-            desired_capabilities = {
+            command_executor='http://0.0.0.0:4723/wd/hub',
+            desired_capabilities={
                 # 'app':app,
                 'platformName': 'iOS',
                 'platformVersion': '12.3.1',
                 'deviceName': 'iPhone',
                 'bundleId': 'bitauto.application',
-                'udid': 'dcfd046369ce2313d16a46c89358df63d336abfe'
+                'udid': '00008020-001E11502E04002E'
             }
         )
 
     def test_push_view(self):
-
-
         next_view_button = self.driver.find_element_by_accessibility_id("entry next view")
         next_view_button.click()
 
@@ -35,6 +31,7 @@ class  appiumSimpleTezt (unittest.TestCase):
     def tearDown(self):
         sleep(1)
     # self.driver.quit()
+
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().loadTestsFromTestCase(appiumSimpleTezt)
